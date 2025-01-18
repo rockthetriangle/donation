723e7e5b279cbd3d6acf21cc30b5903eabe79241
Bilal Atique <bilalbinatique@gmail.com>
2025-01-18 22:23:50 +0500

    fix build errors

--- src/app/events/[event_name]/page.js
+++ src/app/events/[event_name]/page.js
@@ -5,23 +5,17 @@ import BlackShadeSection2 from "@/app/components/main/components/blackshade_sect
 import removeUnderscoresAndCapitalize from "@/app/components/utils/capitalizer";
 import EventPageSection from "@/app/components/main/components/event_page_section";
 
-export async function getStaticPaths() {
-  const readMoreLinksAll = [
+export async function generateStaticParams() {
+  const eventNames = [
     "supporting_hope_a_charity_gala",
     "warmth_in_winter_clothing_drive",
     "crisis_relief_assistance",
   ];
-  const paths = readMoreLinksAll.map((data) => ({
-    params: {
-      event_name: data.toString(),
-    },
+  return eventNames.map((data) => ({
+    event_name: data.toString(),
   }));
-
-  return {
-    paths,
-    fallback: false,
-  };
 }
+
 export default function EventsDetailPage({ params }) {
   const event_name = params.event_name;
   return (
