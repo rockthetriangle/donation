f487d497f9c205e44b4e6db3a088c07ac82ddd61
Bilal Atique <bilalbinatique@gmail.com>
2025-01-21 22:12:38 +0500

    add data in events pages

---
+++ src/app/components/utils/data.js
@@ -0,0 +1,62 @@
+export const EVENTS = [
+  // 1
+  {
+    src: "/img/crisis_relief.jpg",
+    button_color: "emerald-400",
+    title: "Warmth and Hope for Western North Carolina",
+    description:
+      "This winter, we’re delivering essential supplies—coats, blankets, and more—to those devastated and still in great need in Western North Carolina. Together, we can bring comfort and hope to our neighbors.",
+    read_more_link: "/warmth_and_hope",
+    date: "8th FEB, 2025",
+    place: "Canton, North Carolina",
+    contact: "828-555-5555",
+    features: [
+      "Event Feature One",
+      "Event Feature Two",
+      "Event Feature Three",
+      "Event Feature Four",
+      "Event Feature Five",
+      "Event Feature Six",
+    ],
+  },
+  // 2
+  {
+    src: "/img/winter_clothing_drive.jpg",
+    button_color: "cyan-400",
+    title: "Gear Up for Good: Winter Coat & Camping Gear Drive",
+    description:
+      "Help us provide warmth and shelter to those in need! Donate winter coats, sleeping bags, and camping gear to support individuals facing harsh conditions this winter.",
+    read_more_link: "/gear_up_for_good",
+    date: "1st FEB 2025",
+    place: "Loading Dock, Raleigh NC",
+    contact: "828-555-5555",
+    features: [
+      "Event Feature One",
+      "Event Feature Two",
+      "Event Feature Three",
+      "Event Feature Four",
+      "Event Feature Five",
+      "Event Feature Six",
+    ],
+  },
+  // 3
+  {
+    src: "/img/charity_event.jpg",
+    button_color: "sky-400",
+    title: "Clean Streets, Stronger Community",
+    description:
+      "Join us for a local street cleanup to beautify our neighborhoods and show pride in our community. Together, we can make a lasting impact—one street at a time!",
+    read_more_link: "/clean_streets_stronger_community",
+    date: "25th JAN, 2025",
+    place: "TBD, Raleigh NC",
+    contact: "828-555-5555",
+    features: [
+      "Event Feature One",
+      "Event Feature Two",
+      "Event Feature Three",
+      "Event Feature Four",
+      "Event Feature Five",
+      "Event Feature Six",
+    ],
+  },
+];
